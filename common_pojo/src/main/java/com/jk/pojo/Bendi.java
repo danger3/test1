12754228f8eee1400471ec84122f79a13113c935
danger3 <1669411243@qq.com>
2019-07-04 19:11:10 +0800

    测试本地分支

--- common_pojo/src/main/java/com/jk/pojo/Bendi.java
+++ common_pojo/src/main/java/com/jk/pojo/Bendi.java
@@ -3,11 +3,21 @@ package com.jk.pojo;
 public class Bendi {
     private  int pp;
 
+    private  int pp2;
+
     public int getPp() {
         return pp;
     }
 
     public void setPp(int pp) {
         this.pp = pp;
+    }
+
+    public int getPp2() {
+        return pp2;
+    }
+
+    public void setPp2(int pp2) {
+        this.pp2 = pp2;
     }
 }
