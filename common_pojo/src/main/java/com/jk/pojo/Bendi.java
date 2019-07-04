ce209ad912e987f5f850ff275ee26cbb4d785feb
danger3 <1669411243@qq.com>
2019-07-04 17:56:40 +0800

    bendiceshi

---
+++ common_pojo/src/main/java/com/jk/pojo/Bendi.java
@@ -0,0 +1,13 @@
+package com.jk.pojo;
+
+public class Bendi {
+    private  int pp;
+
+    public int getPp() {
+        return pp;
+    }
+
+    public void setPp(int pp) {
+        this.pp = pp;
+    }
+}
